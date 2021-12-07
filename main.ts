7dc17c75c538a2ea6cf556b8819394018075be81
Bobeff123 <93154498+Bobeff123@users.noreply.github.com>
2021-12-07 13:00:33 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,13 @@
 tiles.setTilemap(tilemap`level1`)
 let mySprite = sprites.create(img`
-    7 7 7 7 7 
-    7 f 7 f 7 
-    7 7 7 7 7 
-    7 f f f 7 
-    7 7 7 7 7 
+    8 8 8 8 8 
+    8 f 8 f 8 
+    8 8 8 8 8 
+    8 f f f 8 
+    8 8 8 8 8 
     `, SpriteKind.Player)
 mySprite.ay = 500
+mySprite.ay = 20
+tiles.placeOnTile(mySprite, tiles.getTileLocation(0, 0))
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite, 10, 0)
