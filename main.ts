362373ae61e885f23f746b926c0ac95afbf7f42a
Bobeff123 <93154498+Bobeff123@users.noreply.github.com>
2021-12-07 12:53:52 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
 tiles.setTilemap(tilemap`level1`)
+let mySprite = sprites.create(img`
+    7 7 7 7 7 
+    7 f 7 f 7 
+    7 7 7 7 7 
+    7 f f f 7 
+    7 7 7 7 7 
+    `, SpriteKind.Player)
+mySprite.ay = 500
